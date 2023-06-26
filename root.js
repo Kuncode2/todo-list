b8be7009e8cedc8904888bc176fa2583be9cc72c
kunal <kuncode2@gmail.com>
2023-06-26 22:36:26 +0530

    queryselector vs getelement by id

---
+++ root.js
@@ -0,0 +1,13 @@
+document.querySelector('.push').onclick = function() {
+    if (document.querySelector('.newtask input').value.length == 0) {
+        alert("Please Enter a Task");
+    }
+    
+    else{
+        document.querySelector('.tasks').innerHTML
+        += '<div class ="task">
+        <span id = "taskname">
+            ${}
+        '
+    }
+};
