49273f3e7cbe379e3c3c5e6bd9de9393f2ab60c1
kunal <kuncode2@gmail.com>
2023-06-27 22:10:42 +0530

    inner html property

--- root.js
+++ root.js
@@ -1,13 +1,16 @@
-document.querySelector('.push').onclick = function() {
-    if (document.querySelector('.newtask input').value.length == 0) {
-        alert("Please Enter a Task");
-    }
-    
-    else{
-        document.querySelector('.tasks').innerHTML
-        += '<div class ="task">
-        <span id = "taskname">
-            ${}
-        '
-    }
+document.querySelector(".push").onclick = function () {
+  if (document.querySelector(".newtask input").value.length == 0) {
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector(".tasks").innerHTML += ` 
+             <div class ="task">
+                   <span class="taskname">
+                    ${document.querySelector(".newtask input").value}
+                   </span>
+                   <button class="delete">
+                   <i class="gg-trash"></i>
+                   </button>
+             </div>
+        `;
+  }
 };
